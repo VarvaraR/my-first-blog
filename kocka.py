d2a787a7e6c687a26a4c3195cf740ec9c3e4f89e
Varvara <revavarvara@gmail.com>
2016-10-27 14:25:55 +0200

    My Django Girls app, first commit

---
+++ kocka.py
@@ -0,0 +1,15 @@
+
+def ahoj(name):
+    if name == 'Ola':
+         print('Hey Ola!')
+    elif name == 'Sonja':
+         print('Hey Sonja!')
+    else:
+         print('Hey anonymous!')
+
+ahoj('Jakube')
+
+
+
+
+
